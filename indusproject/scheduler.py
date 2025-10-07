0659f71014f3e13ef10ef7dadca5d2eb0eb3f6e0
Kishore270426 <kishorek2704@gmail.com>
2025-10-07 15:42:32 +0530

    updated scheduler.py

--- indusproject/scheduler.py
+++ indusproject/scheduler.py
@@ -1,33 +1,32 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from apscheduler.triggers.date import DateTrigger
-from datetime import datetime, timedelta
+from datetime import datetime
 from .scrapper import scrape_indus_po_data
 from .status_scrapper import scrape_and_store_in_redis
 
-
-
 def start_scheduler():
-    scheduler = BackgroundScheduler()
+    scheduler = BlockingScheduler()
 
     # Indus PO scraping
     scheduler.add_job(
         func=scrape_indus_po_data,
-        trigger=CronTrigger(hour=13, minute=9),
+        trigger=CronTrigger(hour=16, minute=0),
         id='indus_po_scraper',
-        
         replace_existing=True
     )
 
     # Zepto PO scraping
     scheduler.add_job(
         func=scrape_and_store_in_redis,
-        trigger=CronTrigger(hour=10, minute=38),
+        trigger=CronTrigger(hour=16, minute=45),
         id='scrape_and_store_in_redis',
-        
         replace_existing=True
     )
 
-    scheduler.start()
-    print("[Scheduler] Indus PO scraper scheduled at 11:20 (with retry).")
-    print("[Scheduler] Zepto PO scraper scheduled at 12:40 (with retry).")
+    print("[Scheduler] Indus PO scraper scheduled at 4:00 pm")
+    print("[Scheduler] Zepto PO scraper scheduled at 5:00 pm")
+    
+    scheduler.start()  # <-- blocks and keeps service alive
+
+if __name__ == "__main__":
+    start_scheduler()
